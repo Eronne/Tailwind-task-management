90d0efff6a2d8bb14367ad42b78453d765ede476
Eronne <erwann.letue@gmail.com>
2024-01-10 15:02:36 +0100

    chore(ui): improve layout header

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,7 @@
-<script setup lang="ts">
-const { t } = useI18n()
-const navigation = [
-  { title: t('layouts.default.navigation.home'), to: '/' },
-]
-</script>
-
 <template>
   <div class="bg-white">
     <div class="px-6 py-6 lg:px-8">
-      <Nav :navigation="navigation" />
+      <Nav />
     </div>
   </div>
 </template>
--- components/Nav.vue
+++ components/Nav.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import { Bars3Icon } from '@heroicons/vue/24/outline'
-
-defineProps<{
-  navigation: any[]
-}>()
-
 const { locale, locales } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
 
@@ -19,31 +13,12 @@ const availableLocales = computed(() => {
       <Logo />
     </div>
 
-    <!-- Mobile -->
-    <div class="flex lg:hidden">
-      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-        <span class="sr-only">Open main menu</span>
-        <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-      </button>
-    </div>
-
-    <!-- Desktop -->
-    <div class="flex align-middle">
-      <div class="hidden mr-4 lg:flex lg:gap-x-12">
-        <template v-for="item in navigation" :key="item.title">
-          <NuxtLink :to="item.to" class="text-sm font-semibold leading-6 text-gray-900">
-            {{ item.title }}
-          </NuxtLink>
-        </template>
-      </div>
-
-      <NuxtLink
-        v-for="locale in availableLocales"
-        :key="locale.code"
-        :to="switchLocalePath(locale.code)"
-      >
-        <span class="text-xs uppercase">{{ locale.code }}</span>
-      </NuxtLink>
-    </div>
+    <NuxtLink
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :to="switchLocalePath(locale.code)"
+    >
+      <span class="text-xs uppercase">{{ locale.code }}</span>
+    </NuxtLink>
   </nav>
 </template>
